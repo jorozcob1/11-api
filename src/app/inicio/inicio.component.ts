414e8cecbf5de590d663dbe432b692c1443b8c65
jorozcob1 <jorozcob1@miumg.edu.gt>
2024-07-16 20:11:06 -0600

    mejora

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrl: './inicio.component.css',
 })
 export class InicioComponent {
-
+  title = 'Cocktail Paradise';
 }
